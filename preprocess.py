e8a3299de1a17593b7d4f57d9b69fae041a1be2e
heyyjudes <heyyjudes@outlook.com>
2017-04-09 12:29:11 -0400

    updated with trump and obama tweets

--- preprocess.py
+++ preprocess.py
@@ -1,4 +1,5 @@
 import csv
+import numpy as np
 import HTMLParser
 from nltk.tokenize import TreebankWordTokenizer
 from nltk.stem.porter import *
@@ -23,38 +24,25 @@ def twtt2(input_str):
 
 def twtt3(input_str):
     '''this function removes URLS by splitting string and looking for URL beginnings'''
-    arr = input_str.split(" ")
-    for token in arr:
-        if token.startswith("www") or token.startswith("http"):
-            arr.remove(token)
-    new_str = " ".join(arr)
+
+    new_str = re.sub(r'https?:\/\/.*[\r\n]*', '', input_str)
+    new_str = re.sub(r'www?:\/\/.*[\r\n]*', '', new_str)
+    new_str = re.sub(r'http?:\/\/.*[\r\n]*', '', new_str)
+    new_str = re.sub(r'[#\w.]+twitter.com[\/\w]+', '', new_str)
+
     return new_str
 
 def twtt4(input_str, token_dict, max_len):
     '''remove usernames, hashtags and punctuation at the first character of user names'''
-    clean_txt = re.compile(r'#')
-    new_str = clean_txt.sub('', input_str)
-    clean_txt = re.compile(r'@[\w]+')
-    new_str = clean_txt.sub('', new_str)
 
-    clean_txt = re.compile(r'\'')
-    new_str = clean_txt.sub('', new_str)
-
-    clean_txt = re.compile(r"[-.,!?;*%&<:()'\"\\]+")
-    new_str = clean_txt.sub(' ', new_str)
-
-    clean_txt = re.compile(r"\d+")
-    new_str = clean_txt.sub('', new_str)
+    new_str = re.sub(r"[^A-Za-z]+", ' ', input_str)
 
     tokens = TreebankWordTokenizer().tokenize(new_str.lower())
-    new_str = " ".join(tokens)
-    clean_txt = re.compile(r'\\[\w]+')
-    new_str = clean_txt.sub('', new_str)
-    tokens = new_str.split(" ")
+
     if max_len < len(tokens):
         max_len = len(tokens)
-        print tokens
     token_dict += tokens
+
     return tokens, max_len
 
 def twttpad(tokens, max_size):
@@ -84,8 +72,8 @@ def load_tokens():
 
 if __name__ == "__main__":
     # generate output tweet file with punctuation removed: 1 tweet per line
-    input_path = "data/tweets.csv"
-    output_file = "data/unlabeled_tweet.txt"
+    input_path = "data/BarackObama.csv"
+    output_file = "data/obama_tweet_1000.txt"
     #change this number for number of tweets to include
     num_tweets = 500
 
@@ -96,9 +84,16 @@ if __name__ == "__main__":
         for row in reader:
             my_tweets.append(row)
 
-    pos = my_tweets[:num_tweets]
-    neg = my_tweets[800000:800000+num_tweets]
-    new_tweets = pos + neg
+    #for sentiment tweets
+    # pos = my_tweets[:num_tweets]
+    # neg = my_tweets[800000:800000+num_tweets]
+    # new_tweets = pos + neg
+
+    #for obama and trump tweets
+
+    tweet_indx = np.random.choice(len(my_tweets) - 1, 1000)
+    new_tweets = np.asarray(my_tweets)[tweet_indx]
+
 
     max_len = 31
     token_dict = []
@@ -108,8 +103,7 @@ if __name__ == "__main__":
         final_str = twtt2(final_str)
         final_str = twtt3(final_str)
         tokens, max_len = twtt4(final_str, token_dict, max_len)
-
-        final_str = twttpad(tokens, 31)
+        final_str = twttpad(tokens, max_len)
         final_str = final_str.lstrip(" ")
         out_f.write(final_str + '\n')
 
